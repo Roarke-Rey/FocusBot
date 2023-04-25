2d7fcfaddfc5e09539f3e9252898c68ee1088fe8
Roarke-Rey <shreyas992010@gmail.com>
2023-04-24 22:40:51 -0400

    Added a standalone firebase script for accesing Firebase database with Python

---
+++ firebase.py
@@ -0,0 +1,67 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate("firebase_credentials.json")
+firebase_admin.initialize_app(cred, {
+    "databaseURL": "https://focusbot-c6cfb-default-rtdb.firebaseio.com/"
+})
+
+# Initializing users
+root = db.reference("/")
+
+# Empyting all lists
+# root.child("Users").set({})
+# root.child("Managers").set({})
+
+# Getting handles for users
+users = db.reference("/Users/")
+managers = db.reference("/Managers/")
+
+# # Adding sample data
+user1 = {
+    "Name": "Shreyas", 
+    "Projects": [ 
+    {
+        "Name": "FocusBot",
+        "Tasks":[
+        {
+            "Name": "Sample Task1",
+            "DueDate": "08032023"
+        },
+        {
+            "Name": "Sample Task2",
+            "DueDate": "09032023"
+        }] 
+    }]
+}
+user2 = {
+    "Name": "Jelson", 
+    "Projects": [ 
+    {
+        "Name": "FocusBot",
+        "Tasks":[
+        {
+            "Name": "Sample Task3",
+            "DueDate": "08032023"
+        },
+        {
+            "Name": "Sample Task4",
+            "DueDate": "09032023"
+        }] 
+    }]
+}
+manager1 = {
+    "Name": "Shean", 
+    "Projects": "FocusBot"
+}
+# users.push(user1)
+# users.push(user2)
+# managers.push(manager1)
+
+for key, value in users.get().items():
+    if value["Name"] == "Shreyas":
+        print(value["Projects"][0]["Tasks"])
+        # value["Projects"].push()
+
+# .order_by_child("Due Date")
